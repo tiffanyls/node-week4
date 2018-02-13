1961a118908cb47177f6587f93f5ca6777a74e83
Tiffany Schureman <Tiffany@Tiffanys-MacBook-Air.local>
2018-02-13 11:07:43 -0600

    made cart

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require("express");
 const { json } = require("body-parser");
 const cors = require("cors");
+const session = require('express-session');
 
 const port = 3001;
 
@@ -8,8 +10,17 @@ const app = express();
 
 app.use(json());
 app.use(cors());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    saveUnintialized: false,
+    resave: false,
+    cookie: {maxAge: 10000}
+}));
+
 app.use((req, res, next) =>{
-    console.log('req.method: ', req.method);
+    //console.log('req.method: ', req.method);
+    console.log('SESSION: ', req.session);
+    console.log('BODY:', req.body)
     next();
 });
 
@@ -28,7 +39,18 @@ app.get("/api/test", (req, res, next) => {
 
 app.post('/api/login', checkAuth, (req, res, nest) => {
     res.status(200).json({message: "Authenticated"});
-})
+});
+
+app.post('/api/cart', (req, res, next)=> {
+    if(!req.session.cart) req.session.cart=[];
+    req.session.cart.push(req.body);
+    res.status(200).json(req.session.cart);
+});
+
+app.get('/api/cart', (req, res, next) => {
+    if (!req.session.cart) res.status(400).json({message: "No Cart Created"});
+    else res.status(200).json(req.session.cart);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
